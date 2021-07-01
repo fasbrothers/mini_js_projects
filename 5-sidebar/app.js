fae26b7c38272189c558d585b0fc44bb6ce83ba5
fasbrothers <nigmatovsanjar00@gmail.com>
2021-07-01 19:14:04 +0500

    made 5-sidebar project

---
+++ 5-sidebar/app.js
@@ -0,0 +1,12 @@
+const sidebarToggle = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn')
+const sidebar = document.querySelector('.sidebar')
+
+sidebarToggle.addEventListener('click', ()=>{
+    sidebar.classList.toggle('show-sidebar')
+})
+
+closeBtn.addEventListener('click', ()=>{
+    sidebar.classList.remove('show-sidebar')
+})
+
